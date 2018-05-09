9501e2f5f8a625b3e8c115dbcf0c71d75cf3771c
YoyoCodes <laura.d.petre@gmail.com>
2018-05-09 18:35:24 +0100

    Added interactive menu

--- directory.rb
+++ directory.rb
@@ -92,14 +92,36 @@ def print_less_than_twelve_char_names(students)
     end 
   end
 end
+
+def interactive_menu
+  students = []
+  loop do
+    # 1. print the menu and ask the user what to do
+    puts "1. Input the students"
+    puts "2. Show the students"
+    puts "9. Exit" # 9 because we'll be adding more items
+    # 2. read the input and save it into a variable
+    selection = gets.chomp
+    # 3. do what the user has asked
+    case selection
+    when "1"
+      students = input_students
+    when "2"
+      print_header
+      print(students)
+      print_footer(students)
+    when "9"
+      exit # this will cause the program to terminate
+    else
+      puts "I don't know what you meant, try again"
+    end
+  end
+end
   
 
 #nothing happens until we call the methods
-students = input_students
-print_header
-print(students)
-print_footer(students)
-letter = input_first_letter
-print_with_letter(students, letter)
-print_less_than_twelve_char_names(students)
+interactive_menu
+#letter = input_first_letter
+#print_with_letter(students, letter)
+#print_less_than_twelve_char_names(students)
 	
