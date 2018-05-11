f6198d9b16c77ee27a25560c7dc449a4ccf1ce9c
YoyoCodes <laura.d.petre@gmail.com>
2018-05-11 16:00:41 +0100

    Step 14 - exercise 5; requested user input filename

--- directory.rb
+++ directory.rb
@@ -3,8 +3,8 @@
 def print_menu
   puts "\n1. Input the students"
   puts "2. Show the students"
-  puts "3. Save the list to students.csv"
-  puts "4. Load the list from students.csv"
+  puts "3. Save the list of students" #to students.csv or another file inputed by the user
+  puts "4. Load the list of students.csv" #to students.csv or another file inputed by the user
   puts "9. Exit" # 9 because we'll be adding more items  
 end
 
@@ -22,9 +22,13 @@ def process(selection)
     when "2"
       show_students
     when "3"
-      save_students
+      puts "Please provide a filename to save to (default = students.csv)"
+      filename = STDIN.gets.delete("\n") 
+      save_students(filename)
     when "4"
-     load_students
+     puts "Please provide a filename to load from (default = students.csv)"
+     filename = STDIN.gets.delete("\n") 
+     load_students(filename)
     when "9"
       exit
     else
@@ -130,9 +134,9 @@ def print_footer
   puts "Overall, we have #{@students.count} great students".center(50,"  ") if @students.count > 1    
 end
 
-def save_students
+def save_students(filename)
   # open the file for writing
-  file = File.open("students.csv", "w")
+  file = File.open(filename, "w")
   # iterate over the array of students
   @students.each do |student|
     student_data = [student[:name], student[:cohort], student[:hobby], student[:height]]
@@ -144,7 +148,7 @@ def save_students
   puts "Student list was saved/updated successfully!" 
 end
 
-def load_students(filename = "students.csv")
+def load_students(filename)
   file = File.open(filename, "r")
   file.readlines.each do |line|
     name, cohort, hobby, height = line.chomp.split(',')
